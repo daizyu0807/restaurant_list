d0861239e79ec92e919901dc5e41f99b6443bf58
daizyu0807 <daizyu0807@gmail.com>
2022-01-17 16:11:26 +0800

    feat: modify search function

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json') // 點擊餐廳、查詢餐廳暫時使用
 const Restaurant = require('./models/Restaurant')
 
 mongoose.connect('mongodb://localhost/restaurant_list')
@@ -13,7 +12,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 // app.use(express.urlencoded({ extended: true }))
 
-const db  = mongoose.connection
+const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -21,33 +20,39 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 渲染所有餐廳
+// 所有餐廳
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurantList => res.render('index', { restaurants: restaurantList }))
 })
 
-// 渲染點擊餐廳
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(item => item.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-})
-
-// 渲染查詢餐點
+// 搜尋餐點
 app.get('/search', (req, res) => {
+  if (!req.query.keyword) {
+    res.redirect('/')
+  }
+
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(item => {
-    if (item.name.toLowerCase().includes(keyword.toLowerCase())) {
-      return item.name.toLowerCase().includes(keyword.toLowerCase())
-    } else {
-      return item.category.toLowerCase().includes(keyword.toLowerCase())
-    }
-    
-  })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  Restaurant.find()
+    .lean()
+    .then(restaurantList => {
+      const restaurantFilter = restaurantList.filter(
+        item =>
+          item.name.toLowerCase().includes(keyword.toLowerCase()) ||
+          item.category.toLowerCase().includes(keyword.toLowerCase())
+      )
+      res.render('index', { restaurants: restaurantFilter, keyword: keyword })
+    })
+    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
+})
+
+// 查看餐廳
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(item => item.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
