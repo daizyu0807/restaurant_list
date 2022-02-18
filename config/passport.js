e3f4e4e12ee312d3e4c58a38df52811c99e27263
daizyu0807 <daizyu0807@gmail.com>
2022-02-18 21:42:22 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,8 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
-// const bcrypt = require('bcryptjs')
-// const FacebookStrategy = require('passport-facebook').Strategy
+const bcrypt = require('bcryptjs')
+const FacebookStrategy = require('passport-facebook').Strategy
 module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
@@ -17,15 +17,12 @@ module.exports = app => {
         if (!user) {
           return done(null, false, { type: 'warning_msg', message: '此電子郵件尚未註冊' })
         }
-        if (password === user.password) {
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { type: 'warning_msg', message: '電子郵件或密碼錯誤' })
+          }
           return done(null, user)
-        }
-        // return bcrypt.compare(password, user.password).then(isMatch => {
-        //   if (!isMatch) {
-        //     return done(null, false, { type: 'warning_msg', message: '電子郵件或密碼錯誤' })
-        //   }
-        //   return done(null, user)
-        // })
+        })
       })
       .catch(err => done(err, false))
   }))
@@ -40,27 +37,30 @@ module.exports = app => {
       .catch(err => done(err, null))
   })
   // 引用 Facebook 登入策略
-  // passport.use(new FacebookStrategy({
-  //   clientID: process.env.FACEBOOK_ID,
-  //   clientSecret: process.env.FACEBOOK_SECRET,
-  //   callbackURL: process.env.FACEBOOK_CALLBACK,
-  //   profileFields: ['email', 'displayName']
-  // }, (accessToken, refreshToken, profile, done) => {
-  //   const { name, email } = profile._json
-  //   User.findOne({ email })
-  //     .then(user => {
-  //       if (user) return done(null, user)
-  //       const randomPassword = Math.random().toString(36).slice(-8)
-  //       bcrypt
-  //         .genSalt(10)
-  //         .then(salt => bcrypt.hash(randomPassword, salt))
-  //         .then(hash => User.create({
-  //           name,
-  //           email,
-  //           password: hash
-  //         }))
-  //         .then(user => done(null, user))
-  //         .catch(err => done(err, false))
-  //     })
-  // }))
+  passport.use(new FacebookStrategy({
+    // clientID: process.env.FACEBOOK_ID,
+    // clientSecret: process.env.FACEBOOK_SECRET,
+    // callbackURL: process.env.FACEBOOK_CALLBACK,
+    clientID: '1113198559224345',
+    clientSecret: '57de640d0a045ea10d6f3918c70a7cfb',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
 }
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth') // 掛載 middleware
 
 // 將網址結構符合 / 字串的 request 導向指定模組
 router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
